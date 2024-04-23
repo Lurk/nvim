2a5febd02698e8975cc89c39932a90e65d6e7504
Lurk <bargamon@gmail.com>
2024-04-23 10:11:29 +0200

    change mapping for search all

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -81,7 +81,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', '<leader>si', live_grep, { desc = '[S]earch by grep [i]gnoring gitignore' })
+    vim.keymap.set('n', '<leader>sa', live_grep, { desc = '[S]earch [A]ll by grep ignoring gitignore' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>sb', function()
