d7081bd640a4e2c3d83acc252d76c15af4f9cd44
Lurk <bargamon@gmail.com>
2024-07-16 20:40:51 +0200

    remove eslint-lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -110,7 +110,6 @@ return { -- LSP Configuration & Plugins
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
-      'eslint-lsp',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
